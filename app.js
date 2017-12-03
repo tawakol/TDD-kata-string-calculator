ca30177cc7c2e0bc34c9ba3d0ae0dd0e522bcbae
Dell EMC Devops Workshop <dellemc@ip-172-31-28-47.us-west-2.compute.internal>
2017-12-03 14:21:44 +0000

    commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const routes = require('./routes/index');
+const routes = require('./routes/index'); //commit
 
 const app = express();
 
